6c62ddc473779e4932b5cc123e0d1c09d6ed29ab
Crowdbotics <team@crowdbotics.com>
2022-05-23 22:48:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "milk_and_honey_35412",
+  url: "https://milk_and_honey_35412.botics.co",
+  api: "https://milk_and_honey_35412.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
